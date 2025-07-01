631a0d0fc0e629ab6b61b51f9f628b18fe5b48c3
rolindaz <rolinda.zamboni@gmail.com>
2025-07-01 13:06:54 +0200

    Refactor posts router to use post controller methods for CRUD operations

--- esercizio_bonus/routers/posts.js
+++ esercizio_bonus/routers/posts.js
@@ -7,75 +7,26 @@ const postController = require('../controllers/postController');
 
 // Create - Store
 
-router.post('/', (req, res)=>{
-  console.log('You can add a new post here');
-  res.send("Let's create a new post!");
-});
+router.post('/', postController.store);
 
 // Read - Index
 
-router.get('/', (req, res)=>{
-  console.log("Here's the posts' list");
-  // Creo una variabile per la lista di post filtrati, che inizialmente è uguale alla lista originale
-  let filteredPosts = posts;
-
-  // Se c'è un filtro nella richiesta, filtriamo la lista di post
-  if (req.query.tag) {
-    filteredPosts = posts.filter(
-      post => post.tags.includes(req.query.tag)
-    );
-  };
-
-  // Restituiamo la lista filtrata
-  res.json(filteredPosts);
-});
+router.get('/', postController.index);
 
 // Read - Show
 
-router.get('/:id', (req, res)=>{
-  const id = parseInt(req.params.id);
-  console.log(`Here's the post number ${id}`);
-  const post = posts.find(post => post.id === id);
-  if (!post){
-    return res.status(404).json({
-      error: "Not Found",
-      message: "Post non trovato"
-    });
-  };
-  res.json(post);
-});
+router.get('/:id', postController.show);
 
 // Update - Update
 
-router.put('/:id', (req, res)=>{
-  const id = req.params.id;
-  console.log(`Let's edit the post with id: ${id}`);
-  res.send(`Let's edit the post with id: ${id}`);
-});
+router.put('/:id', postController.update);
 
 // Update - Modify
 
-router.patch('/:id', (req, res)=>{
-  const id = req.params.id;
-  console.log(`Let's edit something about the post with id: ${id}`);
-  res.send(`Let's edit something about the post with id: ${id}`);
-});
+router.patch('/:id', postController.modify);
 
 // Delete - Destroy
 
-router.delete('/:id', (req, res)=>{
-  const id = parseInt(req.params.id);
-  console.log(`Let's delete the post with id: ${id}`);
-  const post = posts.find(post => post.id === id);
-  if (!post){
-    return res.status(404).json({
-      error: "Not Found",
-      message: "Post non trovato"
-    });
-  };
-  posts.splice(posts.indexOf(post), 1);
-  console.log(posts);
-  res.sendStatus(204);
-});
+router.delete('/:id', postController.destroy);
 
 module.exports = router;
