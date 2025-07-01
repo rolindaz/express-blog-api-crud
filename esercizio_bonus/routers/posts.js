06613cb05ed07c984232bc07884de4b6ceda81e2
rolindaz <rolinda.zamboni@gmail.com>
2025-07-01 13:05:02 +0200

    Add post controller import to posts router

--- esercizio_bonus/routers/posts.js
+++ esercizio_bonus/routers/posts.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const posts = require('../data/postsList');
+const postController = require('../controllers/postController');
 
 /* Create CRUD routes */
 
