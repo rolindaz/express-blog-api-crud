2784bb1b25be584bb5c25589cfebfaaf96906a7e
rolindaz <rolinda.zamboni@gmail.com>
2025-07-03 12:47:11 +0200

    Add API token validation to test middleware

--- parte_2/middlewares/testMiddleware.js
+++ parte_2/middlewares/testMiddleware.js
@@ -1,6 +1,16 @@
+// import api token
+
+const apiToken = process.env.apiToken;
+
 function testMiddleware(req, res, next) {
     console.log(`${req.method} - ${req.url}`);
-    next();   
+    if (req.apiToken !== apiToken) {
+        console.log('Request has the wrong apiToken value');
+        res.status(401).json({
+            error: "Not Authorized",
+            message: "You are not authorized to proceed"
+        });
+    } else next();   
 };
 
 module.exports = testMiddleware;
